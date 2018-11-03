88b0d2e0ee0353479aa660bd69c84203e00933ad
j2yongjin <yjlee3854@gmail.com>
2018-11-03 15:43:56 +0900

    개발 네트워크 구성

---
+++ chainbook-network/basic-network/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+docker-compose -f docker-compose.yml down
+
+docker-compose -f docker-compose.yml up -d ca.chainbooks.com orderer.chainbooks.com peer0.org1.chainbooks.com couchdb1 couchdb2 peer0.org2.chainbooks.com
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=10
+#echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
+# Create the channel
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.chainbooks.com/msp" peer0.org1.chainbooks.com peer channel create -o orderer.chainbooks.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+# Join peer0.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.chainbooks.com/msp" peer0.org1.chainbooks.com peer channel join -b mychannel.block
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.chainbooks.com/msp" peer0.org2.chainbooks.com peer channel join -b mychannel.block
---
+++ hyperledger/application-port.md
@@ -0,0 +1,20 @@
+
+
+
+
+## application-network port 정리
+
+   1) 기관1 (DaouBooks)
+       - depend on : 카우치 DB
+   
+   2) 기관2 (DaouToons)
+       - depend on : 카우치 DB
+   
+   3) CA (인증서 발급)
+   
+   4) Orderer (협회)
+   
+   
+## port mapping
+
+
