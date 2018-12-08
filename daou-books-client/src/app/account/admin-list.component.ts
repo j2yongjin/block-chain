dd688bb033123b79b90a199e8be5da03dbb91824
seulgijo <seulgijo@daou.co.kr>
2018-12-08 16:34:17 +0900

    client 개발

--- daou-books-client/src/app/account/admin-list.component.ts
+++ daou-books-client/src/app/account/admin-list.component.ts
@@ -16,23 +16,19 @@ export class AdminListComponent {
     defaultSorting:[[1, "desc"]],
     params:{apitype:"APPR"},
     columns:[{dataName:"회사이름", thClass:"date", dataCode:"companyName", sortable:true, convertData: function(data){
-        return moment(data.createdAt).format("YYYY-MM-DD hh:mm");
+        return data.companyName ? data.companyName : "-";
       }},
       {dataName:"회사코드", thClass:"system", dataCode:"companyCode", sortable:false, convertData: function(data){
-          return data.partnerName ? data.partnerName : "-";
+          return data.companyCode ? data.companyCode : "-";
         }},
       {dataName:"어드민이름", thClass:"system", dataCode:"adminName", sortable:false, convertData: function(data){
-          return data.formName ? data.formName : "-";
+          return data.adminName ? data.adminName : "-";
         }},
-      {dataName:"어드민아이디", thClass:"name", dataCode:"adinmPw", sortable:true, convertData: function(data){
-          return data.actor ? data.actor : "-";
+      {dataName:"어드민아이디", thClass:"name", dataCode:"adminId", sortable:true, convertData: function(data){
+          return data.adminId ? data.adminId : "-";
         }},
       {dataName:"처리상태", thClass:"state", dataCode:"status", sortable:false, convertData: function(data){
-          if(data.status == "SUCCESS"){
-            return "<a class='btn minor'>정상</a>";
-          }else{
-            return "<a class='btn critical'>실패</a>";
-          }
+          return "<a class='btn minor'>상세보기</a>";
         }, clickCallBack: function(pageThis, data){
           let url = "/alliance/appr/log/detail/"+data.id;
           pageThis.router.navigate([url]);}}]
